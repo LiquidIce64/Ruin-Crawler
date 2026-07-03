b964dc2af29d84fd18064bd9fb1b22c1fc9f6a0e
LiquidIce64 <gilyazov.rm2005@gmail.com>
2026-07-03 17:22:24 +0300

    Added gravity volume particles

--- Assets/GameAssets/Scripts/Environment/GravityVolume.cs
+++ Assets/GameAssets/Scripts/Environment/GravityVolume.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class GravityVolume : MonoBehaviour
 {
     [SerializeField] private Vector3 gravityVector = Vector3.zero;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.purple;
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, Vector3.one);
+    }
+
+    private void OnValidate()
+    {
+        var particleSystem = GetComponent<ParticleSystem>();
+        var forceModule = particleSystem.forceOverLifetime;
+        forceModule.xMultiplier = gravityVector.x;
+        forceModule.yMultiplier = gravityVector.y;
+        forceModule.zMultiplier = gravityVector.z;
+    }
 
     private void OnTriggerStay(Collider other)
     {
