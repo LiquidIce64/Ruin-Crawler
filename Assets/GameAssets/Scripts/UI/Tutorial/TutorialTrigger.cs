1aac45212cf01f7dadca631666ee59d5482437f4
LiquidIce64 <gilyazov.rm2005@gmail.com>
2026-07-05 15:38:01 +0300

    Edited tutorial
    Swapped jump and turbo step triggers because it's a better match for the environment around those triggers.
    
    Also changed tutorial trigger to use IVehicleTrigger interface for performance and code cleanliness (good practice to minimize OnTriggerEnter calls by having the player check for triggers instead of every trigger checking for the player)

--- Assets/GameAssets/Scripts/UI/Tutorial/TutorialTrigger.cs
+++ Assets/GameAssets/Scripts/UI/Tutorial/TutorialTrigger.cs
@@ -1,22 +1,13 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class TutorialTrigger : MonoBehaviour
+public class TutorialTrigger : MonoBehaviour, IVehicleTrigger
 {
     [SerializeField] private string stepId;
 
-    private void OnTriggerEnter(Collider other)
+    public void OnTrigger(VehicleController vehicle)
     {
-        VehicleController vehicle = null;
-        if (other.attachedRigidbody != null)
-            vehicle = other.attachedRigidbody.GetComponent<VehicleController>();
-        if (vehicle == null)
-            vehicle = other.GetComponentInParent<VehicleController>();
-
-        if (vehicle != null)
-        {
-            TutorialManager.Instance.RequestStep(stepId);
-            gameObject.SetActive(false);
-        }
+        TutorialManager.Instance.RequestStep(stepId);
+        gameObject.SetActive(false);
     }
 }
